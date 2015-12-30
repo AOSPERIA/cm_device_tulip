8a67fc469b8b896120fa5fed3b03578de7dc4515
jeffreyvh <jeffrey25011998@gmail.com>
2015-12-30 19:35:17 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/aosp_e2303.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/pa_e2303.mk
 
